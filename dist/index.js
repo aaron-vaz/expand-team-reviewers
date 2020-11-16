8135adbed76761b46032781f5bbba9262658caaa
Aaron Vaz <vazaaron08@gmail.com>
2020-11-16 16:38:05 +0000

    fix bug where the same members can be requested multiple times

--- dist/index.js
+++ dist/index.js
@@ -101,56 +101,47 @@ module.exports = /******/ (() => {
           const token = core.getInput("token");
           const client = github.getOctokit(token);
           const payload = context.payload;
+          const requestedReviewers = payload.pull_request.requested_reviewers.map(
+            (reviewer) => reviewer.login
+          );
+          const requestedTeams = payload.pull_request.requested_teams.map(
+            (team) => team.slug
+          );
           core.info("Requesting info for PR");
-          yield client.pulls
-            .get({
+          if (requestedTeams.length === 0) {
+            core.info("No teams reviewers added to this PR");
+            return;
+          }
+          yield Promise.all(
+            requestedTeams.map((team) => {
+              core.info(`Getting members from the following team: ${team}`);
+              return client.teams.listMembersInOrg({
+                org: payload.repository.owner.login,
+                team_slug: team,
+              });
+            })
+          ).then((res) => {
+            const reviewers = res
+              .flatMap((payload) => payload.data)
+              .map((member) => member.login)
+              .filter((member) => !requestedReviewers.includes(member))
+              .filter((login) => login !== payload.pull_request.user.login);
+            if (reviewers.length === 0) {
+              core.info(
+                "Team reviewers are only made up of the author of the PR/Members that are have already been requested"
+              );
+              return;
+            }
+            core.info(
+              `Requesting reviews from the following members: ${reviewers}`
+            );
+            return client.pulls.requestReviewers({
               owner: payload.repository.owner.login,
               repo: payload.repository.name,
               pull_number: payload.pull_request.number,
-            })
-            .then((res) => {
-              const requestedTeams = res.data.requested_teams;
-              if (requestedTeams.length === 0) {
-                core.info("No teams reviewers added to this PR");
-                return;
-              }
-              return Promise.all(
-                requestedTeams
-                  .map((team) => team.slug)
-                  .map((team) => {
-                    core.info(
-                      `Getting members from the following team: ${team}`
-                    );
-                    return client.teams.listMembersInOrg({
-                      org: payload.repository.owner.login,
-                      team_slug: team,
-                    });
-                  })
-              );
-            })
-            .then((res) => {
-              if (!res) {
-                return;
-              }
-              const reviewers = res
-                .flatMap((payload) => payload.data)
-                .map((member) => member.login)
-                .filter((login) => login !== payload.pull_request.user.login);
-              if (reviewers.length === 0) {
-                core.info(
-                  "Team reviewers are only made up of the author of the PR"
-                );
-              }
-              core.info(
-                `Requesting reviews from the following members: ${reviewers}`
-              );
-              return client.pulls.requestReviewers({
-                owner: payload.repository.owner.login,
-                repo: payload.repository.name,
-                pull_number: payload.pull_request.number,
-                reviewers: [...new Set(reviewers)],
-              });
+              reviewers: [...new Set(reviewers)],
             });
+          });
         });
       }
       run().catch(core.setFailed);
--- src/main.ts
+++ src/main.ts
@@ -16,59 +16,52 @@ async function run() {
   const client = github.getOctokit(token);
 
   const payload = context.payload as EventPayloads.WebhookPayloadPullRequest;
+  const requestedReviewers = payload.pull_request.requested_reviewers.map(
+    (reviewer) => reviewer.login
+  );
+  const requestedTeams = payload.pull_request.requested_teams.map(
+    (team: EventPayloads.WebhookPayloadTeamTeam) => team.slug
+  );
 
   core.info("Requesting info for PR");
 
-  await client.pulls
-    .get({
+  if (requestedTeams.length === 0) {
+    core.info("No teams reviewers added to this PR");
+    return;
+  }
+
+  await Promise.all(
+    requestedTeams.map((team) => {
+      core.info(`Getting members from the following team: ${team}`);
+
+      return client.teams.listMembersInOrg({
+        org: payload.repository.owner.login,
+        team_slug: team,
+      });
+    })
+  ).then((res) => {
+    const reviewers = res
+      .flatMap((payload) => payload.data)
+      .map((member) => member.login)
+      .filter((member) => !requestedReviewers.includes(member)) // filter out reviewers that have already been requested
+      .filter((login) => login !== payload.pull_request.user.login); // filter out author of PR
+
+    if (reviewers.length === 0) {
+      core.info(
+        "Team reviewers are only made up of the author of the PR/Members that are have already been requested"
+      );
+      return;
+    }
+
+    core.info(`Requesting reviews from the following members: ${reviewers}`);
+
+    return client.pulls.requestReviewers({
       owner: payload.repository.owner.login,
       repo: payload.repository.name,
       pull_number: payload.pull_request.number,
-    })
-    .then((res) => {
-      const requestedTeams = res.data.requested_teams;
-
-      if (requestedTeams.length === 0) {
-        core.info("No teams reviewers added to this PR");
-        return;
-      }
-
-      return Promise.all(
-        requestedTeams
-          .map((team) => team.slug)
-          .map((team) => {
-            core.info(`Getting members from the following team: ${team}`);
-
-            return client.teams.listMembersInOrg({
-              org: payload.repository.owner.login,
-              team_slug: team,
-            });
-          })
-      );
-    })
-    .then((res) => {
-      if (!res) {
-        return;
-      }
-
-      const reviewers = res
-        .flatMap((payload) => payload.data)
-        .map((member) => member.login)
-        .filter((login) => login !== payload.pull_request.user.login); // filter out author of PR
-
-      if (reviewers.length === 0) {
-        core.info("Team reviewers are only made up of the author of the PR");
-      }
-
-      core.info(`Requesting reviews from the following members: ${reviewers}`);
-
-      return client.pulls.requestReviewers({
-        owner: payload.repository.owner.login,
-        repo: payload.repository.name,
-        pull_number: payload.pull_request.number,
-        reviewers: [...new Set(reviewers)], // the same members can be in multiple teams so we only get a list of unique members
-      });
+      reviewers: [...new Set(reviewers)], // the same members can be in multiple teams so we only get a list of unique members
     });
+  });
 }
 
 run().catch(core.setFailed);
